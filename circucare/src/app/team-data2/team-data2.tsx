8a86c4b9f2ad4aacd864efeb4652c2f048046f69
jayygao <jug015@ucsd.edu>
2024-07-13 00:05:15 -0700

    navbar redesign + team page update w/ styling & div name changes

---
+++ circucare/src/app/team-data2/team-data2.tsx
@@ -0,0 +1,11 @@
+const data2 = [
+    { name2: "Michael Schilling", src2: "/blankphoto.webp", text2: "Frontend Designer" , link2: "https://www.linkedin.com/in/michaelschilling/"},
+    { name2: "Jane Doe", src2: "/blankphoto.webp", text2: "Frontend Designer", link2: "https://www.google.com/"},
+    { name2: "ADAD Doe", src2: "/blankphoto.webp", text2: "Frontend Designer" , link2: "https://www.google.com/"},
+    { name2: "JaADADne Doe", src2: "/blankphoto.webp", text2: "Frontend Designer", link2: "https://www.google.com/"},
+    { name2: "JaADADne Doe", src2: "/blankphoto.webp", text2: "Frontend Designer", link2: "https://www.google.com/"},
+    { name2: "JaADADne Doe", src2: "/blankphoto.webp", text2: "Frontend Designer", link2: "https://www.google.com/"},
+    { name2: "JaADADne Doe", src2: "/blankphoto.webp", text2: "Frontend Designer", link2: "https://www.google.com/"}
+]
+
+export default data2
--- circucare/src/app/team/page.tsx
+++ circucare/src/app/team/page.tsx
@@ -3,33 +3,55 @@ import React from 'react'
 import './team.css'
 import NavBar from './../navbar/navbar'
 import data from '../team-data/team-data'
+import inputData from '../team-data2/team-data2'
 
 export default function teamPage() {
     const images = data;
+    const images2 = inputData;
 
     return (
-        <div className = "main-container">
+        <div className = "background-container">
             <div className = "top-bar">
                 <NavBar/>
             </div>
-            <div className ="sub-container">
-                <div className = "textInfo">
-                    <h1> Meet the Team </h1>
-                </div>
-                <div className = "dataInput">
-                    {
-                        images.map(data => (
-                            <div className='cards'>
-                                <div className = 'teamNames'>
-                                    {data.name}
+            <div className = "main-container">
+                <div className ="sub-container">
+                    <div className = "title-main">
+                        <h1> Advisory Board </h1>
+                    </div>
+                    <div className = "main-cards">
+                        {
+                            images.map(data => (
+                                <div className='cards-container'>
+                                    <div className = 'teamNames'>
+                                        {data.name}
+                                    </div>
+                                    <a href={data.link}><img src={data.src} alt="photos" className='teamImages'/></a>
+                                    <div className = 'teamData'>
+                                        {data.text}
+                                    </div>
                                 </div>
-                                <a href={data.link}><img src={data.src} alt="photos" className='teamImages'/></a>
-                                <div className = 'teamData'>
-                                    {data.text}
+                            ))
+                        }
+                    </div>
+                    <div className = "title-secondary">
+                        <h1> Excutive Team </h1>
+                    </div>
+                    <div className = "sub-cards">
+                        {
+                            images2.map(inputData => (
+                                <div className='sub-cards-container'>
+                                    <div className = 'teamNames-2'>
+                                        {inputData.name2}
+                                    </div>
+                                    <a href={inputData.link2}><img src={inputData.src2} alt="photos2" className='teamImages-2'/></a>
+                                    <div className = 'teamData-2'>
+                                        {inputData.text2}
+                                    </div>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </div>
